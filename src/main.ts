57208dabd75fd1f3de5e024785a00c25c72f1648
Tamta <tamtakopadz16@gmail.com>
2024-08-05 23:15:21 +0400

    update port

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from "./swagger";
-import helmet from "helmet";
-import { ValidationPipe } from "@nestjs/common";
-import { GlobalExceptionsFilter } from "./common/exceptions/GlobalExceptionsFilter";
-import { loggerMiddleware } from "./middlewares";
+import { setupSwagger } from './swagger';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionsFilter } from './common/exceptions/GlobalExceptionsFilter';
+import { loggerMiddleware } from './middlewares';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api");
+  app.setGlobalPrefix('api');
   app.use(helmet());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new GlobalExceptionsFilter());
   app.use(loggerMiddleware);
   const document = setupSwagger(app);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 
 bootstrap();
